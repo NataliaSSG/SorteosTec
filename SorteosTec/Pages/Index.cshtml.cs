dbdb23bc2f5d3392e9ad9fd4fe3523945a76e479
Sylvia Aurora <osky_cortes@hotmail.com>
2023-10-19 13:57:08 -0600

    Added Login settings

--- SorteosTec/Pages/Index.cshtml.cs
+++ SorteosTec/Pages/Index.cshtml.cs
@@ -1,10 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace SorteosTec.Pages;
 
 public class IndexModel : PageModel
 {
+    [BindProperty]
+    public string Username { get; set; }
+    [BindProperty]
+    public string Password { get; set; }
+    public string Result {get; set;}
+    public string Conn = $"Server={http://localhost:3306}; Database={DBName}; User={Username}; Password={Password}";
+    public bool Login()
+    {
+        MySqlConnection connection = new MySqlConnection(Conn);
+        try 
+        { 
+            
+            connection.Open();
+            return true;
+        } catch (Exception ex)
+        {
+
+        }
+        finally 
+        {
+            connection.Close();
+            
+        }
+    }
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
