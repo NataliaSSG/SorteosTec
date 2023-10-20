adddef514c4c82c49abfc4a7888728e84df832da
RosasSebastian2003 <111817772+rosassebastian2@users.noreply.github.com>
2023-10-20 00:55:50 -0600

    Inicio de sesion con diccionario funcional

--- SorteosTec/Pages/Index.cshtml.cs
+++ SorteosTec/Pages/Index.cshtml.cs
@@ -1,41 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using MySql.Data.MySqlClient;
+// using MySql.Data.MySqlClient;
 
 namespace SorteosTec.Pages;
 
 public class IndexModel : PageModel
 {
-    [BindProperty]
-    public string Username { get; set; }
-    [BindProperty]
-    public string Password { get; set; }
-    public string Result {get; set;}
-    public string Conn = $"Server={http://localhost:3306}; Database={DBName}; User={Username}; Password={Password}";
-    public bool Login()
-    {
-        MySqlConnection connection = new MySqlConnection(Conn);
-        try 
-        { 
-            
-            connection.Open();
-            return true;
-        } catch (Exception ex)
-        {
-
-        }
-        finally 
-        {
-            connection.Close();
-            
-        }
-    }
+    [BindProperty] //Incluimos el tag para que Index.cshtml sea capaz de acceder a las propiedades de UserDetailsModel
     public UserDetailsModel UserDetails {get; set;}
-    private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    // public string Conn = $"Server={http://localhost:3306}; Database={DBName}; User={Username}; Password={Password}";
+    // public bool Login()
+    // {
+    //     MySqlConnection connection = new MySqlConnection(Conn);
+    //     try 
+    //     { 
+            
+    //         connection.Open();
+    //         return true;
+    //     } catch (Exception ex)
+    //     {
+
+    //     }
+    //     finally 
+    //     {
+    //         connection.Close();
+            
+    //     }
+    // }
+
+    public IndexModel( )
     {
-        _logger = logger;
+
+        UserDetails = new UserDetailsModel(); //Objeto de la clase UserDetailsModel, para accdeer a sus propiedades
     }
 
     public void OnGet()
@@ -50,7 +47,18 @@ public class IndexModel : PageModel
             return Page();
         }
 
-        return RedirectToPage("/Home");
+        Console.WriteLine($"Username: {UserDetails.Username}");
+        Console.WriteLine($"Password: {UserDetails.Password}");
+
+        bool containsUsername = UserDetails.testDict.ContainsKey(UserDetails.Username);
+        if (containsUsername && UserDetails.testDict[UserDetails.Username] == UserDetails.Password)
+        {
+            return RedirectToPage("/Home");
+        }
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Usuario o contraseña incorrectos");
+            return Page();
+        }
     }
 }
-
--- SorteosTec/Pages/UserDetailsModel.cshtml.cs
+++ SorteosTec/Pages/UserDetailsModel.cshtml.cs
@@ -1,10 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 public class UserDetailsModel 
 {
+    [BindProperty]
     [Required(ErrorMessage = "Inserte su nombre de usuario por favor")]
     public string Username { get; set; }
-
-     [Required(ErrorMessage = "Inserte su contraseña por favo")]
+    
+    [BindProperty]
+    [Required(ErrorMessage = "Inserte su contraseña por favor")]
     public string Password { get; set; }
+
+    public Dictionary<string, string> testDict { get; set; }
+
+    public UserDetailsModel() //Inicializamos el diccionario dentro de el constructor de la clase
+    {
+        testDict = new Dictionary<string, string>()
+        {
+            {"root", "root"},
+        };
+    }
 }
