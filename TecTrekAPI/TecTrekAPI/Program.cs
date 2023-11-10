8710ee7fdee00f2e5c3668aa5266bd8c3de853d6
RosasSebastian2003 <rosassebastian2003@gmail.com>
2023-11-09 18:14:56 -0600

    API fix pt 2

--- TecTrekAPI/TecTrekAPI/Program.cs
+++ TecTrekAPI/TecTrekAPI/Program.cs
@@ -1,14 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using TecTrekAPI.Data;
-using TecTrekAPI.Models;
-using Microsoft.OpenApi.Services;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddDbContext<dbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("dbContext"), 
-    new MySqlServerVersion(new Version(8, 1, 0)))); 
+// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
