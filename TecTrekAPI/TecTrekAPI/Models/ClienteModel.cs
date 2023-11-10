60b548677048a9e4eb91c207cbad5ee41184595d
RosasSebastian2003 <rosassebastian2003@gmail.com>
2023-11-09 18:46:42 -0600

    merged changes, API Incomplete

---
+++ TecTrekAPI/TecTrekAPI/Models/ClienteModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TecTrekAPI.Models
+{
+	public class ClienteModel
+	{
+		public int Id { set; get; }
+		public String Nombre { set; get; }
+		public String Apellido { set; get; }
+		public DateTime FechaNacimiento { set; get; }
+		public int Genero { set; get; }
+		public String Correo { set; get; }
+		public String Username { set; get; }
+
+		public ClienteModel()
+		{
+
+		}
+	}
+}
+
