f51f1ef4acf40bedc0d60b78a874d5a7e3feea83
RosasSebastian2003 <rosassebastian2003@gmail.com>
2023-11-15 15:35:36 -0600

    First test finished

--- Tests/APITests/Data/dbContextTest.cs
+++ Tests/APITests/Data/dbContextTest.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using TecTrekAPI.Models;
+using TecTrekAPI.Data;
 
-namespace Tests.APITests.Data
+namespace TecTrekAPI.Tests
 {
-	public class dbContextTest
+	public class dbContextTests
 	{
-		private readonly TecTrekWebApplicationFactory<Startup> _factory;
-		public dbContextTest()
+		[Fact]
+		public async Task CanInsertIntoDatabase()
 		{
+			var options = new DbContextOptionsBuilder<dbContext>()
+				.UseInMemoryDatabase(databaseName: "TestDB")
+				.Options;
+
+			using (var context = new dbContext(options))
+			{
+				var testClient = new ClienteModel { id_client = 1, first_name = "Test", last_name = "Client", birth_date = new System.DateTime(2021, 1, 1), sexo = 1, email = "test@mail.com", admin = false, user_password = "123456" };
+				context.client.Add(testClient);
+				await context.SaveChangesAsync();
+
+				var clientFromDb = context.client.FirstOrDefault(c => c.id_client == testClient.id_client);
+				Assert.NotNull(clientFromDb);
+				// Assert other properties here
+			}
 		}
+
+		// Add other tests here
 	}
 }
