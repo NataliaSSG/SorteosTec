95f87dec36916234d860a078d99169dee761fc3f
NataliaSSG <nataliasalgado2205@gmail.com>
2023-11-28 17:57:52 -0600

    hola

--- SorteosTec/wwwroot/SQL/TecTrek2.sql
+++ SorteosTec/wwwroot/SQL/TecTrek2.sql
@@ -16,8 +16,8 @@ FLUSH PRIVILEGES;
 -- 			vvvvvvvvvvvvvvv
 --  >>>>>>>>mysqldump -u master -h localhost --port=3306 --protocol=TCP TecTrek -p > <Agrega/Tu/Path>/dbdump.sql <<<<<<<<<<
 -- 			^^^^^^^^^^^^^^^
-CREATE table client(
-	id_client INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE table Client(
+	id_Client INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	username varchar(40) NOT NULL,
 	first_name varchar(30) NOT NULL,
 	last_name varchar(30) NOT NULL,
@@ -40,52 +40,52 @@ CREATE table items(
 
 CREATE table log_user(
 	id_log int NOT NULL auto_increment primary key,
-	id_client INT NOT NULL,
+	id_Client INT NOT NULL,
 	log_in timestamp NOT NULL,
 	log_out timestamp NOT NULL,
 	points int NOT NULL,
-	foreign key (id_client) references client(id_client)
+	foreign key (id_Client) references Client(id_Client)
 );
 
 
 CREATE table address(
 	id_address int NOT NULL auto_increment primary key,
-	id_client INT NOT NULL,
+	id_Client INT NOT NULL,
 	state_name varchar(50) NOT NULL,
 	city_name varchar(50) NOT NULL,
-	foreign key (id_client) references client(id_client)
+	foreign key (id_Client) references Client(id_Client)
 );
 
 
 CREATE table add_ons(
 	id_add_on int NOT NULL auto_increment primary key,
-	id_client INT NOT NULL,
+	id_Client INT NOT NULL,
 	coins bigint NOT NULL,
 	extra_lives int NOT NULL,
 	immunity int NOT NULL,
 	current_skin int NOT NULL,
-	foreign key (id_client) references client(id_client)
+	foreign key (id_Client) references Client(id_Client)
 );
 
 
 CREATE table user_inventory(
 	id_inventory int NOT NULL auto_increment primary key,
-	id_client INT NOT NULL,
+	id_Client INT NOT NULL,
 	id_item int NOT NULL,
 	quantity int,
-	foreign key (id_client) references client(id_client),
+	foreign key (id_Client) references Client(id_Client),
 	foreign key (id_item) references items(id_item)
 );
 
 
 CREATE table transactions(
 	id_transaction int NOT NULL auto_increment primary key,
-	id_client INT NOT NULL,
+	id_Client INT NOT NULL,
 	id_item int NOT NULL,
 	quantity int NOT NULL,
 	payment_type bool NOT NULL,
 	transaction_date timestamp NOT NULL,
-	foreign key (id_client) references client(id_client),
+	foreign key (id_Client) references Client(id_Client),
 	foreign key (id_item) references items(id_item)
 );
 
@@ -105,17 +105,15 @@ VALUES
 
 
 
-INSERT INTO client (username, first_name, last_name, birth_date, user_password, email, sexo, points, role)
+INSERT INTO Client (username, first_name, last_name, birth_date, user_password, email, sexo, points, role)
 VALUES
-('Nataliaaa', 'Natalia', 'Salgado', '2002-05-22', '696969', 'natsg@gmail.com',1, 500, 'admin'),
-('SylviaPoyito', 'Sylvia', 'Cortes', '2000-07-07', '070707', 'sylviac@hotmail.com',2, 750, 'admin'),
-('Sebs', 'Sebastian', 'Rosas', '2003-02-06', '420420', 'sebs@gmail.com',0, 501, 'admin'),
-('Yu', 'Yudith', 'Palacios', '2003-08-07', '50069', 'yuyu@hotmail.com',1, 600, 'admin'),
-('Shaqx', 'Isaac', 'Enriquez', '1999-01-20', '123456','shax@hotmail.com',0, 1530, 'client'),
-('AOkay', 'Narharis', 'Narharinio', '2001-01-01', 'nananabatman', 'aokei@gmail.com',0, 90, 'client');
+('Nataliaaa', 'Natalia', 'Salgado', '2002-05-22', '696969', 'natsg@gmail.com',1, 500, 'Admin'),
+('SylviaPoyito', 'Sylvia', 'Cortes', '2000-07-07', '070707', 'sylviac@hotmail.com',2, 750, 'Admin'),
+('Sebs', 'Sebastian', 'Rosas', '2003-02-06', '420420', 'sebs@gmail.com',0, 501, 'Admin'),
+('Yu', 'Yudith', 'Palacios', '2003-08-07', '50069', 'yuyu@hotmail.com',1, 600, 'Admin'),
+('Shaqx', 'Isaac', 'Enriquez', '1999-01-20', '123456','shax@hotmail.com',0, 1530, 'Client'),
+('AOkay', 'Narharis', 'Narharinio', '2001-01-01', 'nananabatman', 'aokei@gmail.com',0, 90, 'Client');
 
 
 create view leaderboard as
-select username, points from client order by points desc;
-
-select * from client;
+select username, points from Client order by points desc;
